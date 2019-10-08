55d9fc3216419587717ae0f06371ec16a939b0e6
Hutch Crowley <tothecoda@gmail.com>
2019-10-08 18:41:29 -0500

    updtate

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+// select nav elements
+
 const nav = document.getElementsByClassName('nav-link').item(0);
 
 const nav1 = document.getElementsByClassName('nav-link').item(1);
@@ -6,59 +9,88 @@ const nav2 = document.getElementsByClassName('nav-link').item(2);
 
 const nav3 = document.getElementsByClassName('nav-link').item(3);
 
+// change background color of a nav element
 
 nav.addEventListener('click', (Event) => {
     Event.target.style.backgroundColor = 'green';
 });
 
+// select text area - logs selected text to the console
+
 const textarea = document.querySelector('textarea');
 
 textarea.onselect = (event) => {
     const selection = (event.target.value.substring(event.target.selectionStart, event.target.selectionEnd));
-   let newSelection =  `You selected: ${selection}!`;
+    let newSelection = `You selected: ${selection}!`;
 
-   console.log(newSelection);
+    console.log(newSelection);
 };
 
-
-
+// onclick event that changes the background color of nav1
 
 nav1.addEventListener('click', (Event) => {
     Event.target.style.backgroundColor = 'green';
+    Event.stopPropagation();
+    Event.preventDefault();
+
 });
+
+// onclick event that changes the font color of nav2
 
 nav2.addEventListener('click', (Event) => {
+    Event.target.style.color = 'green';
+});
+
+// mouseover event that changes the background color of nav3
+
+nav3.addEventListener('onmouseover', (Event) => {
     Event.target.style.backgroundColor = 'green';
 });
 
-nav3.addEventListener('click', (Event) => {
-    Event.target.style.backgroundColor = 'green';
-});
+// mouseover event that changes the backgroundcolor of the first nav element
 
-nav.addEventListener('mouseover', (Event) => {
+nav.addEventListener('onmouseover', (Event) => {
     Event.target.style.backgroundColor = 'red';
-    });
+});
 
-    nav1.addEventListener('mouseover', (Event) => {
-        Event.target.style.backgroundColor = 'red';
-        });
+// same for nav1
+
+nav1.addEventListener('onmouseover', (Event) => {
+    Event.target.style.backgroundColor = 'red';
+});
+
+// on focus event that changes the background color of nav1
 
 nav1.addEventListener('focus', (Event) => {
-Event.target.style.backgroundColor = 'blue';
+    Event.target.style.backgroundColor = 'blue';
 });
 
-nav2.addEventListener('mouseover', (Event) => {
+// mouseover event for changing background color of nav2
+
+nav2.addEventListener('onmouseover', (Event) => {
     Event.target.style.backgroundColor = 'red';
-    });
+    Event.stopPropagation();
+});
 
-    nav3.addEventListener('mouseover', (Event) => {
-        Event.target.style.backgroundColor = 'red';
-        });
+// another mouseover event for nav3
 
+nav3.addEventListener('onmouseover', (Event) => {
+    Event.target.style.backgroundColor = 'red';
+    Event.stopPropagation();
+});
+
+// onload event that console logs 'Fun Bus Has Loaded' when the window object finishes loading
 
 const windowObj = window;
 
 const load = () => {
-    alert('Fun Bus has Loaded!');
+    console.log('Fun Bus has Loaded!');
 };
 window.onload = load;
+
+const navAll = document.querySelectorAll('nav');
+
+navAll.addEventListener('click', (Event) => {
+
+    Event.preventDefault();
+});
