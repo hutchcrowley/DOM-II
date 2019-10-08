d87e17f764a39c0363dc6afb85e6d4995f595c54
Hutch Crowley <tothecoda@gmail.com>
2019-10-08 00:19:26 -0500

    init commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
-// Your code goes here
+const nav = document.getElementsByClassName('nav-link').item(0);
+
+const nav1 = document.getElementsByClassName('nav-link').item(1);
+
+const nav2 = document.getElementsByClassName('nav-link').item(2);
+
+const nav3 = document.getElementsByClassName('nav-link').item(3);
+
+nav.addEventListener('click', (Event) => {
+    Event.target.style.backgroundColor = 'green';
+});
+
+nav1.addEventListener('click', (Event) => {
+    Event.target.style.backgroundColor = 'green';
+});
+
+nav2.addEventListener('click', (Event) => {
+    Event.target.style.backgroundColor = 'green';
+});
+
+nav3.addEventListener('click', (Event) => {
+    Event.target.style.backgroundColor = 'green';
+});
+
+nav.addEventListener('mouseover', (Event) => {
+    Event.target.style.backgroundColor = 'red';
+    });
+
+    nav1.addEventListener('mouseover', (Event) => {
+        Event.target.style.backgroundColor = 'red';
+        });
+
+nav1.addEventListener('focus', (Event) => {
+Event.target.style.backgroundColor = 'blue';
+});
+
+nav2.addEventListener('mouseover', (Event) => {
+    Event.target.style.backgroundColor = 'red';
+    });
+
+    nav3.addEventListener('mouseover', (Event) => {
+        Event.target.style.backgroundColor = 'red';
+        });
+
+const selectAll = document.getElementsByClassName('.container-home');
+
+selectAll.addEventListener('scroll', (Event) => {
+    Event.target.scroll() (0%; 1200; smooth);
+});
